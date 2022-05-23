ec333336a9cbc65f72110cacf2af7650322ac7c8
alantheandroid <alessio.ilardi@gmail.com>
2022-05-23 18:07:48 +0200

    feat: add 'createStore' function

--- exercise-3.js
+++ exercise-3.js
@@ -1,16 +1,22 @@
 function createStore() {
-  // ...
+  let store = [];
+  return function (item) {
+    store.push(item);
+    return store;
+  };
 }
 
-const redPants = { id: 1, name: 'Red Pants' };
-const whiteHat = { id: 2, name: 'White Hat' };
-const blackSneakers = { id: 3, name: 'Black Sneakers' };
+const redPants = { id: 1, name: "Red Pants" };
+const whiteHat = { id: 2, name: "White Hat" };
+const blackSneakers = { id: 3, name: "Black Sneakers" };
 
-console.log('--- Dress Store ---');
+console.log("--- Dress Store ---");
 const dressStore = createStore();
 dressStore(redPants);
-dressStore(whiteHat)
+const fullDressStore = dressStore(whiteHat);
+console.log(fullDressStore);
 
-console.log('--- Shoes Store ---');
+console.log("--- Shoes Store ---");
 const shoesStore = createStore();
-shoesStore(blackSneakers);
+const fullShoesStore = shoesStore(blackSneakers);
+console.log(fullShoesStore);
